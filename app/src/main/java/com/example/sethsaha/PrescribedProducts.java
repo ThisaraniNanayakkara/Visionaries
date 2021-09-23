8756018e01856f4970f33c8097f3c46a42007c4d
IT20269784 <IT20269784@my.sliit.lk>
2021-09-23 17:20:43 +0530

    my first UIs

---
+++ app/src/main/java/com/example/sethsaha/PrescribedProducts.java
@@ -0,0 +1,14 @@
+package com.example.sethsaha;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PrescribedProducts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.prescribed_products);
+    }
+}
