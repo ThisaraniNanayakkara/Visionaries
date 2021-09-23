d8d02483c8b834a7e3f034304438c08cdedec7ce
ThisaraniNanayakkara <it20047788@my.sliit.lk>
2021-09-23 15:12:01 +0530

    Initial commit

---
+++ app/src/main/java/com/mad2021/sethsaha/Login.java
@@ -0,0 +1,34 @@
+package com.mad2021.sethsaha;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//import com.mad2021.sethsaha.database.DBHelper;
+
+public class Login extends AppCompatActivity {
+
+//    EditText edtName, edtPassword;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+//        edtName = findViewById(R.id.edtName);
+//        edtPassword = findViewById(R.id.edtPassword);
+    }
+
+//    public void saveUser(View view){
+//        String name =edtName.getText().toString();
+//        String password = edtPassword.getText().toString();
+//        DBHelper dbHelper = new DBHelper(this);
+//
+//        if(name.isEmpty() || password.isEmpty()){
+//            Toast.makeText(this, "Enter values", Toast.LENGTH_SHORT).show();
+//        }else{
+//            dbHelper.addInfo(name,password);
+//        }
+//    }
+}
---
+++ app/src/main/java/com/mad2021/sethsaha/MainActivity.java
@@ -0,0 +1,24 @@
+package com.mad2021.sethsaha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+//import com.mad2021.sethsaha.database.DBHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
