f0254f902dc6bf3920f9b321617e1fb7937dcd69
IT20269784 <IT20269784@my.sliit.lk>
2021-09-26 17:47:20 +0530

    Update

--- app/src/main/java/com/example/sethsaha/ProfileManagement.java
+++ app/src/main/java/com/example/sethsaha/ProfileManagement.java
@@ -1,8 +1,13 @@
 package com.example.sethsaha;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class ProfileManagement extends AppCompatActivity {
 
@@ -10,5 +15,29 @@ public class ProfileManagement extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profilemanagement);
+    }
+
+    public void DeleteAccount(View view) {
+        ShowDialog();
+    }
+    private void ShowDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(ProfileManagement.this);
+        builder.setTitle("Delete");
+        builder.setMessage("Are you sure about deleting the profile?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                documentReference.delete()
+                        .addOnSuccessListener(new OnSuccessListener<Void>(){
+
+                            @Override
+                            public void onSuccess(Void unused) {
+                                public void onSuccess(Void aVoid){
+
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
